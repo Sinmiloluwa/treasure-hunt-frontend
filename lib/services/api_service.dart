ae9865c467f1fa56a37aebd166dd3989ac3bec9c
Sinmiloluwa <mofeoluwae@gmail.com>
2025-09-29 18:39:36 +0100

    updated api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,9 +1,9 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:treasure_hunt/services/session_service.dart';
 
 class ApiService {
-  // Define your API service methods and properties here
-  final String baseUrl;
-  ApiService(this.baseUrl);
+  static const String baseUrl = 'https://treasure-hunt-backend-yfl1.onrender.com/api';
 
   static Future<Map<String, String>> _getHeaders(
       {bool includeAuth = true}) async {
@@ -19,6 +19,50 @@ class ApiService {
     }
 
     return headers;
+  }
+
+  static Future<http.Response> get(String endpoint,
+      {Map<String, String>? headers}) async {
+    final requestHeaders = await _getHeaders();
+
+    if (headers != null) {
+      requestHeaders.addAll(headers);
+    }
+
+    final uri = Uri.parse('$baseUrl$endpoint');
+
+    return await http.get(uri, headers: requestHeaders);
+  }
+
+  static Future<http.Response> post(String endpoint,
+      {Map<String, dynamic>? body}) async {
+    final headers = await _getHeaders();
+    final uri = Uri.parse('$baseUrl$endpoint');
+
+    return await http.post(
+      uri,
+      headers: headers,
+      body: body != null ? json.encode(body) : null,
+    );
+  }
+
+  static Future<http.Response> put(String endpoint,
+      {Map<String, dynamic>? body}) async {
+    final headers = await _getHeaders();
+    final uri = Uri.parse('$baseUrl$endpoint');
+
+    return await http.put(
+      uri,
+      headers: headers,
+      body: body != null ? json.encode(body) : null,
+    );
+  }
+
+  static Future<http.Response> delete(String endpoint) async {
+    final headers = await _getHeaders();
+    final uri = Uri.parse('$baseUrl$endpoint');
+
+    return await http.delete(uri, headers: headers);
   }
 
 }
